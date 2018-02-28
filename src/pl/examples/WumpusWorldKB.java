9e8aaf2dfb3684868999b84e0cc806a373af4f23
Ali Hashim <ahashim@u.rochester.edu>
2018-02-28 15:32:23 -0500

    Run config

--- src/pl/examples/WumpusWorldKB.java
+++ src/pl/examples/WumpusWorldKB.java
@@ -2,6 +2,7 @@ package pl.examples;
 
 import pl.core.*;
 import pl.prover.BasicModelChecking;
+import pl.prover.WalkSAT;
 
 public class WumpusWorldKB extends KB {
 	
@@ -36,6 +37,10 @@ public class WumpusWorldKB extends KB {
 		}else{
 			System.out.println("There is not a pit at location 1, 2");
 		}
+
+		WalkSAT wSAT = new WalkSAT();
+
+		wSAT.solve(kb).dump();
 	}
 
 }
